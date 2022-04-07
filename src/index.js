15ff431ed0e87b093062e691c786f316f5a2d74c
sabrybabi <sabrinabongiovanni96@gmail.com>
2022-04-07 17:35:47 +0200

    added grid and js functions

--- src/index.js
+++ src/index.js
@@ -26,3 +26,59 @@ function showSlides() {
   setTimeout(showSlides, 4000); // Change image every 6 seconds
 }
 
+const quickViewButtons = document.querySelectorAll('[data-quick-view');
+const closeButtons = document.querySelectorAll('[data-close]');
+const fullwidthCards = document.querySelectorAll('.fullwidth');
+
+let toggle;
+let toggleParent;
+let fullwidth;
+
+const openQuickView = (toggle, toggleParent, fullwidth) => {
+  toggle.setAttribute('aria-expanded', 'true');
+  toggleParent.classList.toggle('is-selected');
+  fullwidth.classList.toggle('is-hidden');
+  fullwidth.setAttribute('tabIndex', '0');
+};
+
+const closeQuickView = (toggle, toggleParent, fullwidth) => {
+  toggle.setAttribute('aria-expanded', 'false');
+  toggleParent.classList.toggle('is-selected');
+  fullwidth.classList.toggle('is-hidden');
+  fullwidth.removeAttribute('tabIndex');
+};
+
+quickViewButtons.forEach(quickView => {
+  fullwidth = quickView.parentElement.nextElementSibling;
+  quickView.setAttribute('aria-expanded', 'false');
+  quickView.setAttribute('aria-controls', fullwidth.id);
+
+  quickView.addEventListener('click', (e) => {
+    toggle = e.target;
+    toggleParent = toggle.parentElement;
+    fullwidth = toggleParent.nextElementSibling;
+
+    if (toggle.getAttribute('aria-expanded') === 'false') {
+      fullwidthCards.forEach(fullwidthOpen => {
+        if (!fullwidthOpen.classList.contains('is-hidden')) {
+          toggleParentOpen = fullwidthOpen.previousElementSibling;
+          toggleOpen = toggleParentOpen.querySelector('[data-quick-view]');
+          closeQuickView(toggleOpen, toggleParentOpen, fullwidthOpen);}
+        });
+        openQuickView(toggle, toggleParent, fullwidth);
+      } else {
+        closeQuickView(toggle, toggleParent, fullwidth);
+      }
+      });
+    });
+
+    closeButtons.forEach(close => {
+      close.addEventListener('click', (e) => {
+          fullwidth = e.target.parentElement;
+          toggleParent = e.target.parentElement.previousElementSibling;
+          toggle = toggleParent.querySelector('[data-quick-view]');
+  
+          closeQuickView(toggle, toggleParent, fullwidth);
+          toggle.focus(); 
+      });
+  });
